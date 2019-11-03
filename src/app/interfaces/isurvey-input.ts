bf7283f20dbb11970114c4f7bd05e160f4579797
Koji Matae <kmatae@pitadigi.jp>
2019-11-03 12:28:21 +0900

    20191103

--- src/app/interfaces/isurvey-input.ts
+++ src/app/interfaces/isurvey-input.ts
@@ -2,40 +2,50 @@ export interface ISurveyInput {
     // 社員はやる気に満ちており、元気に働いている
     motivation: string;
     motivationTitle: string;
+    motivationKey: string;
 
     // 机の上、机の周りはきれいに整頓されている
     cleanDesk: string;
     cleanDeskTitle: string;
+    cleanDeskKey: string;
 
     // 社内の業務はマニュアル化しており、必要な社員で共有できている
     manual: string;
     manualTitle: string;
+    manualKey: string;
 
     // 業務にかかっている時間を把握し、改善に活かしている
     timeManagement: string;
     timeManagementTitle: string;
+    timeManagementKey: string;
 
     // テレワーク、在宅ワークができる環境を整備し、実際に利用している社員がいる
     telework: string;
     teleworkTitle: string;
+    teleworkKey: string;
 
     // 社員同士の連絡は、主に「○○○」で行うことが多い
     communication: string;
     communicationTitle: string;
+    communicationKey: string;
 
     // 社内の情報は、主に「○○○」で共有している
     informationSharing: string;
     informationSharingTitle: string;
+    informationSharingKey: string;
 
     // 社内手続は、主に「○○○」で行っている
     workflow: string;
     workflowTitle: string;
+    workflowKey: string;
 
     // 顧客情報は、顧客の属性、顧客に付随する情報(案件、問合せなど)を含め一元管理できている
     customerManagement: string;
     customerManagementTitle: string;
+    customerManagementKey: string;
 
     // 売上、経費、利益の状況は最新をすぐに確認できるようになっている
     profitManagement: string;
     profitManagementTitle: string;
+    profitManagementKey: string;
 }
--- src/app/models/survey-input.ts
+++ src/app/models/survey-input.ts
@@ -3,61 +3,51 @@ import { ISurveyInput } from '../interfaces/isurvey-input';
 export class SurveyInput implements ISurveyInput {
     // 社員はやる気に満ちており、元気に働いている
     motivation: string;
-    get motivationTitle(): string {
-        return '社員はやる気に満ちており、元気に働いている';
-    }
+    motivationTitle = '社員はやる気に満ちており、元気に働いている';
+    motivationKey = 'motivation';
 
     // 机の上、机の周りはきれいに整頓されている
     cleanDesk: string;
-    get cleanDeskTitle(): string {
-        return '机の上、机の周りはきれいに整頓されている';
-    }
+    cleanDeskTitle = '机の上、机の周りはきれいに整頓されている';
+    cleanDeskKey = 'cleanDesk';
 
     // 社内の業務はマニュアル化しており、必要な社員で共有できている
     manual: string;
-    get manualTitle(): string {
-        return '社内の業務はマニュアル化しており、必要な社員で共有できている';
-    }
+    manualTitle = '社内の業務はマニュアル化しており、必要な社員で共有できている';
+    manualKey = 'manual';
 
     // 業務にかかっている時間を把握し、改善に活かしている
     timeManagement: string;
-    get timeManagementTitle(): string {
-        return '業務にかかっている時間を把握し、改善に活かしている';
-    }
+    timeManagementTitle = '業務にかかっている時間を把握し、改善に活かしている';
+    timeManagementKey = 'timeManagement';
 
     // テレワーク、在宅ワークができる環境を整備し、実際に利用している社員がいる
     telework: string;
-    get teleworkTitle(): string {
-        return 'テレワーク、在宅ワークができる環境を整備し、実際に利用している社員がいる';
-    }
+    teleworkTitle =  'テレワーク、在宅ワークができる環境を整備し、実際に利用している社員がいる';
+    teleworkKey =  'telework';
 
     // 社員同士の連絡は、主に「○○○」で行うことが多い
     communication: string;
-    get communicationTitle(): string {
-        return '社員同士の連絡は、主に「○○○」で行うことが多い';
-    }
+    communicationTitle = '社員同士の連絡は、主に「○○○」で行うことが多い';
+    communicationKey = 'communication';
 
     // 社内の情報は、主に「○○○」で共有している
     informationSharing: string;
-    get informationSharingTitle(): string {
-        return '社内の情報は、主に「○○○」で共有している';
-    }
+    informationSharingTitle = '社内の情報は、主に「○○○」で共有している';
+    informationSharingKey = 'informationSharing';
 
     // 社内手続は、主に「○○○」で行っている
     workflow: string;
-    get workflowTitle(): string {
-        return '社内手続は、主に「○○○」で行っている';
-    }
+    workflowTitle = '社内手続は、主に「○○○」で行っている';
+    workflowKey = 'workflow';
 
     // 顧客情報は、顧客の属性、顧客に付随する情報(案件、問合せなど)を含め一元管理できている
     customerManagement: string;
-    get customerManagementTitle(): string {
-        return '顧客情報は、顧客の属性、顧客に付随する情報(案件、問合せなど)を含め一元管理できている';
-    }
+    customerManagementTitle = '顧客情報は、顧客の属性、顧客に付随する情報(案件、問合せなど)を含め一元管理できている';
+    customerManagementKey = 'customerManagement';
 
     // 売上、経費、利益の状況は最新をすぐに確認できるようになっている
     profitManagement: string;
-    get profitManagementTitle(): string {
-        return '売上、経費、利益の状況は最新をすぐに確認できるようになっている';
-    }
+    profitManagementTitle = '売上、経費、利益の状況は最新をすぐに確認できるようになっている';
+    profitManagementKey = 'profitManagement';
 }
